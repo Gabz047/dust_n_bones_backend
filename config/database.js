56499b058ded07c91b323da0a7b100c06df3f27d
Gabz047 <gabe1612mail@gmail.com>
2025-10-24 11:23:30 -0300

    FEAT: Force using ipv4

--- config/database.js
+++ config/database.js
@@ -4,34 +4,36 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    timezone: '-03:00',
-    dialectOptions: {
-        useUTC: false,
-        timezone: 'America/Sao_Paulo',
-        ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dialect: process.env.DB_DIALECT,
+  logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    dialect: process.env.DB_DIALECT,
-    logging: process.env.NODE_ENV === 'development' ? console.log : false,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    define: {
-        timestamps: true,
-        underscored: true,
-        freezeTableName: true
+    options: {
+      host: process.env.DB_HOST,
+      family: 4 
     }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true
+  }
 });
+
 
 const Connect = async () => {
     try {
