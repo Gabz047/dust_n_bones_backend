3561391c277a8f8762c764ef6a3f6fd973fa66b6
Gabz047 <gabe1612mail@gmail.com>
2025-10-24 11:37:24 -0300

    FIX: fix database config

--- config/database.js
+++ config/database.js
@@ -3,45 +3,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  dialect: process.env.DB_DIALECT,
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // Render/Postgres remoto aceita assim
     },
-    options: {
-      host: process.env.DB_HOST,
-      family: 4 
-    }
   },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  define: {
-    timestamps: true,
-    underscored: true,
-    freezeTableName: true
-  }
 });
 
-
 const Connect = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('✅ Conexão com o banco de dados estabelecida com sucesso.');
-    } catch (error) {
-        console.error('❌ Erro ao conectar com o banco de dados:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Conexão com o banco de dados estabelecida com sucesso.');
+  } catch (error) {
+    console.error('❌ Erro ao conectar com o banco de dados:', error);
+  }
 };
 
 export { Connect };
